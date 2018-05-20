5b22b1ccc0dec96e5c043c6a8cb42b403c87a6d5
joakin <joaquin@chimeces.com>
2018-05-20 10:57:45 +0200

    Transpile for older browsers and build

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
   devtool: "source-map",
   module: {
     rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
       {
         test: /\.svg$/,
         loader: "svg-inline-loader",
