ad5db7add9c1afe8bb7333fbdbdb64650031aa34
Joakin <joaquin@chimeces.com>
2018-09-05 19:52:02 +0200

    Initial JS and Elm app setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 // Elm output will be prepended to this file.
 // ------------------------------------------
-console.log(Elm);
+window.ContextCards = (function() {
+  var LINK_SELECTOR = "a[data-wiki-title]";
+  var contextCardsApp = null;
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var div = document.createElement("div");
+    document.body.appendChild(div);
+    contextCardsApp = Elm.ContextCards.init({ node: div });
+    bindLinks();
+  });
+
+  function bindLinks() {
+    var links = document.querySelectorAll(LINK_SELECTOR);
+
+    for (var i = 0; i < links.length; i++) {
+      var link = links[i];
+
+      if (link.dataset.wikiPreviewEventsBound !== "events-set") {
+        link.dataset.wikiPreviewEventsBound = "events-set";
+
+        link.addEventListener("mouseenter", function(event) {
+          var link = event.target;
+          var title = link.dataset.wikiTitle;
+          var lang = link.dataset.wikiLang;
+
+          console.log("mouseenter", title, " ", lang);
+        });
+        link.addEventListener("mouseout", function(event) {
+          var link = event.target;
+          var title = link.dataset.wikiTitle;
+          var lang = link.dataset.wikiLang;
+
+          console.log("mouseout", title, " ", lang);
+        });
+      }
+    }
+  }
+
+  return {
+    bindLinks: bindLinks
+  };
+})();
