6ffd8ce61ff2dbcb8331ae8145a420c2a3809f88
Joakin <joaquin@chimeces.com>
2018-09-07 12:40:12 +0200

    Link & preview entering and leaving with timeouts UX

--- src/ContextCards.elm
+++ src/ContextCards.elm
@@ -1,9 +1,13 @@
 port module ContextCards exposing (main)
 
 import Browser
-import Html exposing (Html, div, text)
-import Html.Attributes exposing (class, style)
+import Html exposing (Html, div, node, text)
+import Html.Attributes exposing (class, classList, style)
+import Html.Events exposing (onMouseEnter, onMouseLeave)
+import Html.Keyed as Keyed
 import Json.Decode as D
+import Process
+import Task
 
 
 main =
@@ -16,26 +20,33 @@ main =
 
 
 type Model
-    = Idle
-    | ActiveLink Link
+    = Idle (Maybe Link)
+    | Active Link InteractionStatus
+
+
+type InteractionStatus
+    = ActiveLink
+    | LeavingLink
+    | ActivePreview
+    | LeavingPreview
 
 
 type Msg
-    = MouseEventOnLink MouseEvent
+    = LinkEnter Link
+    | LinkLeave Link
+    | LinkLeaveTimeout Link
+    | PreviewEnter Link
+    | PreviewLeave Link
+    | PreviewLeaveTimeout Link
 
 
 type alias Link =
     { lang : String
     , title : String
-    , link : D.Value
+    , domElement : D.Value
     , rect : ClientRect
     , scroll : Scroll
     }
-
-
-type MouseEvent
-    = Enter Link
-    | Leave Link
 
 
 type alias ClientRect =
@@ -56,53 +67,109 @@ type alias Scroll =
 
 init : () -> ( Model, Cmd Msg )
 init () =
-    ( Idle, Cmd.none )
+    ( Idle Nothing, Cmd.none )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case msg of
-        MouseEventOnLink event ->
-            case event of
-                Enter link ->
-                    ( ActiveLink link, Cmd.none )
+    case ( model, msg ) of
+        ( _, LinkEnter link ) ->
+            ( Active link ActiveLink, Cmd.none )
 
-                Leave link ->
-                    case model of
-                        Idle ->
-                            ( model, Cmd.none )
+        ( Active currentLink ActiveLink, LinkLeave link ) ->
+            if currentLink.domElement == link.domElement then
+                ( Active link LeavingLink, timeout (LinkLeaveTimeout link) )
 
-                        ActiveLink currentLink ->
-                            if currentLink.link == link.link then
-                                ( Idle, Cmd.none )
+            else
+                model |> noCmds
 
-                            else
-                                ( model, Cmd.none )
+        ( Active currentLink LeavingLink, LinkLeaveTimeout link ) ->
+            if currentLink.domElement == link.domElement then
+                idle link
+
+            else
+                model |> noCmds
+
+        ( Active currentLink LeavingLink, PreviewEnter link ) ->
+            if currentLink.domElement == link.domElement then
+                Active link ActivePreview |> noCmds
+
+            else
+                model |> noCmds
+
+        ( Active currentLink LeavingPreview, PreviewEnter link ) ->
+            if currentLink.domElement == link.domElement then
+                Active link ActivePreview |> noCmds
+
+            else
+                model |> noCmds
+
+        ( Active currentLink ActivePreview, PreviewLeave link ) ->
+            if currentLink.domElement == link.domElement then
+                ( Active link LeavingPreview, timeout (PreviewLeaveTimeout link) )
+
+            else
+                model |> noCmds
+
+        ( Active currentLink LeavingPreview, PreviewLeaveTimeout link ) ->
+            if currentLink.domElement == link.domElement then
+                idle link
+
+            else
+                model |> noCmds
+
+        ( _, _ ) ->
+            model |> noCmds
+
+
+noCmds model =
+    ( model, Cmd.none )
+
+
+idle link =
+    ( Idle (Just link), Cmd.none )
+
+
+timeout msg =
+    Process.sleep 300 |> Task.perform (\() -> msg)
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ case model of
-            Idle ->
-                text ""
+    let
+        viewLink link dismissed =
+            [ ( link.lang ++ " " ++ link.title
+              , viewCard link dismissed
+              )
+            ]
+    in
+    Keyed.node "div"
+        []
+    <|
+        [ ( "styles", node "style" [] [ text styles ] ) ]
+            ++ (case model of
+                    Idle Nothing ->
+                        []
 
-            ActiveLink link ->
-                viewCard link
-        ]
+                    Idle (Just lastLink) ->
+                        viewLink lastLink True
+
+                    Active link interactionStatus ->
+                        viewLink link False
+               )
 
 
-viewCard : Link -> Html Msg
-viewCard link =
+viewCard : Link -> Bool -> Html Msg
+viewCard link dismissed =
     div
-        [ class "ContextCard"
-        , style "position" "absolute"
+        [ classList
+            [ ( "ContextCard", True )
+            , ( "ContextCardDismissed", dismissed )
+            ]
         , style "top" (px (link.rect.top + link.scroll.y + link.rect.height))
         , style "left" (px (link.rect.left + link.scroll.x))
-        , style "z-index" "10000"
-        , style "background-color" "white"
-        , style "padding" "1em"
-        , style "box-shadow" "0 0 20px rgba(0,0,0,0.1)"
+        , onMouseEnter (PreviewEnter link)
+        , onMouseLeave (PreviewLeave link)
         ]
         [ text link.title ]
 
@@ -111,9 +178,52 @@ px n =
     String.fromFloat n ++ "px"
 
 
+styles =
+    """
+    @keyframes contextCardsFadeIn {
+        from {
+            opacity: 0;
+            transform: translate3d(0, 50%, 0);
+        }
+
+        to {
+            opacity: 1;
+            transform: translate3d(0, 0, 0);
+        }
+    }
+    @keyframes contextCardsFadeOut {
+        from {
+            opacity: 1;
+            transform: translate3d(0, 0, 0);
+        }
+
+        to {
+            opacity: 0;
+            transform: translate3d(0, 50%, 0);
+        }
+    }
+
+    .ContextCard {
+        position: absolute;
+        z-index: 10000;
+        background-color: white;
+        padding: 1em;
+        box-shadow: 0 0 20px rgba(0,0,0,0.1);
+        animation-name: contextCardsFadeIn;
+        animation-delay: 500ms;
+        animation-duration: 300ms;
+        animation-fill-mode: both;
+    }
+    .ContextCard.ContextCardDismissed {
+        animation-name: contextCardsFadeOut;
+        animation-delay: 0ms;
+    }
+    """
+
+
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    mouseEvent (MouseEventOnLink << mouseEventJsonToMouseEvent)
+    mouseEvent mouseEventJsonToMouseEvent
 
 
 type alias MouseEventJson =
@@ -126,22 +236,22 @@ type alias MouseEventJson =
     }
 
 
-mouseEventJsonToMouseEvent : MouseEventJson -> MouseEvent
+mouseEventJsonToMouseEvent : MouseEventJson -> Msg
 mouseEventJsonToMouseEvent json =
     let
         link =
             { lang = json.lang
             , title = json.title
-            , link = json.link
+            , domElement = json.link
             , rect = json.rect
             , scroll = json.scroll
             }
     in
     if json.kind == "enter" then
-        Enter link
+        LinkEnter link
 
     else
-        Leave link
+        LinkLeave link
 
 
 port mouseEvent : (MouseEventJson -> msg) -> Sub msg
